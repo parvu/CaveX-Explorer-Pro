7359c47d314bbc7be25aae00ee8e49987012231c
parvu <petrisor.parvu@upb.ro>
2026-08-05 11:15:27 +0300

    Vendor and build ardupilot_gazebo (ArduPilotPlugin for Gazebo Harmonic)

---
+++ ros2_ws/ardupilot_gazebo_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export GZ_SIM_SYSTEM_PLUGIN_PATH=/home/parvu/CaveX-Explorer-Pro/.worktrees/cavex-tracked-blueboat-ardupilot/ardupilot_gazebo/build:$GZ_SIM_SYSTEM_PLUGIN_PATH
+export GZ_SIM_RESOURCE_PATH=/home/parvu/CaveX-Explorer-Pro/.worktrees/cavex-tracked-blueboat-ardupilot/ardupilot_gazebo/models:/home/parvu/CaveX-Explorer-Pro/.worktrees/cavex-tracked-blueboat-ardupilot/ardupilot_gazebo/worlds:$GZ_SIM_RESOURCE_PATH
